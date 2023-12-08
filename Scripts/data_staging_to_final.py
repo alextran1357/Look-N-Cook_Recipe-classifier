a2d6a546cb336129f463ea0ef392827bed563a17
Alex Tran <alextran1357@gmail.com>
2023-12-08 13:21:33 -0800

    Add staging file with scrript to move data

---
+++ Scripts/data_staging_to_final.py
@@ -0,0 +1,15 @@
+import csv
+
+source_csv_path = "data/binary_class_senences-stage.csv"
+target_csv_path = "data/binary_class_senences.csv"
+
+# Open the target CSV file in append mode
+with open(target_csv_path, 'a', newline='', encoding='utf-8') as target_file:
+    writer = csv.writer(target_file)
+
+    # Open and read the source CSV file
+    with open(source_csv_path, 'r', newline='', encoding='utf-8') as source_file:
+        reader = csv.reader(source_file)
+        # Append each row from the source file to the target file
+        for row in reader:
+            writer.writerow(row)
