0e70becbf254b14c4c02b700fca7b309020f3c65
Alex Tran <alextran1357@gmail.com>
2023-12-08 10:50:02 -0800

    implement vocab list sorter for easier navigation

---
+++ Scripts/cooking_vocab_sorter.py
@@ -0,0 +1,25 @@
+# Access verb text file for read
+file_name = "data/Cooking words/cooking_verbs.txt"
+with open(file_name, 'r') as file:
+    cooking_verbs = [line.strip() for line in file]
+
+# Acess noun text file for read
+file_name = "data/Cooking words/cooking_nouns.txt"
+with open(file_name, 'r') as file:
+    cooking_nouns = [line.strip() for line in file]
+
+# Sorting
+cooking_verbs.sort()
+cooking_nouns.sort()
+
+# Access verb text file for read
+file_name = "data/Cooking words/cooking_verbs.txt"
+with open(file_name, 'w') as file:
+    for item in cooking_verbs:
+        file.write("%s\n" % item)
+        
+# Acess noun text file for read
+file_name = "data/Cooking words/cooking_nouns.txt"
+with open(file_name, 'w') as file:
+    for item in cooking_nouns:
+        file.write("%s\n" % item)
